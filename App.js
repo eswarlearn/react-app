e5ceba883e499a37a52b22444ace164f77e6d262
ESWARAMOORTHY M <me4717187@gmail.com>
2024-10-02 08:48:16 +0530

    episode 1.90

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading = React.createElement("h1", {id:"hdng"}, " hello world by react");
 // //{} it a object to give attribute to the class
 // console.log(heading);//heading is a object not html object
@@ -11,7 +13,7 @@ const nested = React.createElement("div",
     {id:"parent"},
     [React.createElement("div",
         {id:"child"},
-        [ React.createElement("h1",{},"i am h1"),React.createElement("h2",{},"i am h2")]
+        [ React.createElement("h1",{},"i am h1..."),React.createElement("h2",{},"i am h2")]
     ),
     React.createElement("div",
         {id:"child2"},
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # React 
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hard Module Replacement
+- File waatching algorithm - written in c++
+- Catching - Faster Buildinds
+- Image optimization
+- Minification 
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error handling
+- can host on http and https
+- Tree Shaking - remove the unused code
+- Different dev and prod bundler
