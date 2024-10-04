74c955987f53b3695c0189b926f280038eab3002
ESWARAMOORTHY M <me4717187@gmail.com>
2024-10-04 08:49:25 +0530

    episode 3 end

--- App.js
+++ App.js
@@ -9,19 +9,43 @@ import ReactDOM from "react-dom/client";
 // // render method is converting object to html tag
 
 
-const nested = React.createElement("div",
-    {id:"parent"},
-    [React.createElement("div",
-        {id:"child"},
-        [ React.createElement("h1",{},"i am h1..."),React.createElement("h2",{},"i am h2")]
-    ),
-    React.createElement("div",
-        {id:"child2"},
-        [ React.createElement("h1",{},"i am h1"),React.createElement("h2",{},"i am h2")]
-    )]
-)
+// const nested = React.createElement("div",
+//     {id:"parent"},
+//     [React.createElement("div",
+//         {id:"child"},
+//         [ React.createElement("h1",{},"i am h1..."),React.createElement("h2",{},"i am h2")]
+//     ),
+//     React.createElement("div",
+//         {id:"child2"},
+//         [ React.createElement("h1",{},"i am h1"),React.createElement("h2",{},"i am h2")]
+//     )]
+// )
 
-console.log(nested)
-const root = ReactDOM.createRoot(document.getElementById("root"))
+// Rect Element
 
-root.render(nested);
+
+const reaEle = <h1 id="jsxHead">element</h1>;
+
+// React functional component
+const JsxHeading = () => {
+    return <h1 id="jsxHead">JSX syntax in React</h1>;
+};
+
+const str = 'from js';
+
+// React functional component
+const HeadingComponent = () => {
+    return (
+        <div>
+            <JsxHeading />
+            <h1 className="component">Component Composition</h1>
+            <h2>{100 + 200}</h2>
+            { reaEle }
+            {/* We can use JS inside {} */}
+        </div>
+    );
+};
+
+// Create root and render
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
